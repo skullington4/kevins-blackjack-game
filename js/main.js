139cc0a1737661bcc7575fb0a5fd3478cd89a24e
skullington4 <ksully121888@gmail.com>
2023-02-05 09:09:50 -0500

    layout updated. Updated some logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import "./cardsMain";
 /*------------ Constants --------------*/
 const PLAYER = {
     p: '1',
@@ -19,6 +20,9 @@ let wager = 0;
 let turn = 1;
 let pHand = [];
 let cHand = [];
+let shuffledDeck = [];
+let pBlackjack = null;
+let cBlackjack = null;
 
 
 /*------------ cached elements --------------*/
@@ -31,11 +35,11 @@ const computerCardsEl = document.getElementById('#cCards');
 const playerCardsEl = document.getElementById('#pCards');
 const hitEl = document.getElementById('#hit');
 const StayEl = document.getElementById('#stay');
-
+const StartGameEl = document.getElementById('#placeWager');
 
 
 /*------------ Event listeners --------------*/
-
+StartGameEl.addEventListener('click', playHand)
 
 
 /*------------ Functions --------------*/
@@ -45,23 +49,55 @@ init();
 
 function init() {
     turn = 1;
-    
+
     winner = null;
-    pHand = [null, null, null, null, null];
-    cHand = [null, null, null, null, null];
+    pHand = [null, null, null, null, null, null, null, null, null, null];
+    cHand = [null, null, null, null, null, null, null, null, null, null];
+    winnerEl.style.visibility = "hidden";
+    shuffledDeck = new getNewShuffledDeck();
     render();
 }
 
 function render() {
+    renderHand();
+    renderScore();
+}
+
+
+function renderHand(hand, player) {
+    // This function should interate through each players hand,
+    // update their respective score values, and updates the score divs
+    let cardsInHand = "";
+    hand.forEach(function(card) {
+        cardsInHand += `<div class="card ${card.face}"></div>`;
+      });
+
 
 }
 
 
+function renderScore (hand, player) {
+    //This logic will include shuffledDeck[0-i].value to propogate the value
+    // Thinking this will be a while loop
+    
+
+}
+
+
+function playHand() {
+    // This function is going to hold the logic for the whole individual hand
+
+    // Dealing the firsh 4 cards
+    pHand = shuffledDeck.shift();
+    cHand = shuffledDeck.shift();
+    pHand = shuffledDeck.shift();
+    cHand = shuffledDeck.shift();
+    render();
 
 
 
 
-
+}
 
 
 
